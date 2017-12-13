0abff947be6612a384143346a88d5c5159a23ca3
kenmatsumoto <ken@nri.com>
2017-12-13 12:47:36 -0800

    Notification channel setting

--- app/src/main/java/com/example/kenmatsumoto/fcmtestclient/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/kenmatsumoto/fcmtestclient/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package com.example.kenmatsumoto.fcmtestclient;
 
+import android.app.NotificationChannel;
 import android.util.Log;
 
 import com.firebase.jobdispatcher.Job;
@@ -7,6 +8,18 @@ import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.app.NotificationManager;
+import android.app.Notification.Builder;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+
+import android.widget.TextView;
+import android.app.Activity;
 
 import static android.content.ContentValues.TAG;
 
@@ -20,22 +33,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
+
+
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (/* Check if data needs to be processed by long running job */ true) {
+            if (/* Check if data needs to be processed by long running job */ false) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
                 scheduleJob();
             } else {
                 // Handle message within 10 seconds
-                handleNow();
+                sendNotification(remoteMessage.getData().toString());
+                handleNow(remoteMessage);
             }
 
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
+            sendNotification(remoteMessage.getNotification().getBody());
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
@@ -43,7 +60,34 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // message, here is where that should be initiated. See sendNotification method below.
     }
 
-    private void handleNow() {
+    private void sendNotification(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0 , intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        NotificationChannel channel = new NotificationChannel(
+                "news",
+                "Coffee Order",
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "news")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Coffee Ordering")
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
+
+        notificationManager.notify(0 , notificationBuilder.build());
+    }
+
+    private void handleNow(RemoteMessage remoteMessage) {
         Log.d(TAG, "shor lived task is done.");
     }
 
